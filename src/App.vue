cf773d5c01c31187eff97689013c91f4696aca6c
Steve-Morales <sam008@ucsd.edu>
2022-06-18 18:45:45 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import './../node_modules/Bulma/CSS/bulma.css';
+
+import NavBar from './components/NavBar.vue';
+</script>
+
+<template>
+  <div>
+    <NavBar>
+
+    </NavBar>
+  </div>
+
+
+  <section class="section">
+
+
+    <div class="card">
+
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" width="1280" height="960">
+        </figure>
+      </div>
+
+      <div class="card-content is-overlay">
+          <!-- some content to be placed over the image -->
+          content ontop
+      </div>
+
+    </div>
+
+
+  </section>
+</template>
+
+<style scoped>
+.bulma-overlay-mixin {
+  @include overlay(1.5rem);
+  background-color: darkorange;
+  border-radius: 0.25em;
+  color: white;
+  opacity: 0.9;
+  padding: 1em;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import './../../node_modules/Bulma/CSS/bulma.css';
+</script>
+
+<template>
+    <!-- 
+        https://bulma.io/documentation/components/navbar/
+     -->
+    <nav class="navbar is-black">
+
+
+        <div class="navbar-brand">
+            <!-- Note: must add 'https://' otherwise it assumes you are going to page within the website -->
+            <a class="level" href="https://example.com">
+                <img src="https://mindbodymoms.com/wp-content/uploads/2018/06/Your-Logo-here.png" alt="Aero Logo" width="45" height="15">
+            </a>
+        </div>
+
+
+        <div class="navbar-menu">
+
+            <div class="navbar-start">
+                <a class="navbar-item" href="https://example.com"> Home </a>
+                <a class="navbar-item" href="https://example.com"> About </a>
+                <a class="navbar-item" href="https://example.com"> Drones </a>
+                <a class="navbar-item" href="https://example.com"> Charging Stations </a>
+                <a class="navbar-item" href="https://example.com"> Our Team </a>
+                <a class="navbar-item" href="https://example.com"> Contact </a>
+            </div> <!-- navbar-start -- end -->
+
+        </div> <!-- navbar-menu -- end -->
+
+
+    </nav>
+</template>
+
+<style scoped>
+.navbar {
+    flex:1;
+    display: flex;
+    align-content: center;
+    justify-content: center;
+}
+
+.navbar-menu {
+    flex-grow: initial;
+    flex-shrink: initial;
+}
+</style>
