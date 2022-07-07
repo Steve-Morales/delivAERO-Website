9dc2f17500f2f034b4bf5883f1579dd0a6f72420
Steve-Morales <sam008@ucsd.edu>
2022-07-06 19:00:43 -0700

    Tab Descriptors Updated

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@ import NavBar from './components/NavBar.vue';
 import ContactForm from './components/ContactForm.vue'
 import Footer from './components/Footer.vue'
 import HomePageSection from './components/HomePageSection.vue'
+
+document.title = "delivAERO"
 </script>
 
 <template>
