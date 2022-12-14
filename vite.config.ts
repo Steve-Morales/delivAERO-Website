bae1ea8b5abf7e22354e237fb3b0925f2dcba85f
Steve-Morales <sam008@ucsd.edu>
2022-12-14 01:21:16 -0800

    fixed base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/delivAERO-Website/'
+  base: '/delivAERO-Website/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
